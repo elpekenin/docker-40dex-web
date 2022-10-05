2b73eab85bab4e18e487a857323214eb5b54c0e2
elpekenin <monopatinct@gmail.com>
2022-10-05 03:07:28 +0200

    Add cluster timeout

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ logging.basicConfig(
 logging.info("Started !!")
 
 if config.db_uri:
-    client = MongoClient(config.db_uri)
+    client = MongoClient(config.db_uri, serverSelectionTimeoutMS=5000)
 else:
     client = MongoClient(
         config.db_ip,
