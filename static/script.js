96cd11cc6dd9d35553854b6c773d7af8cecdc971
elpekenin <monopatinct@gmail.com>
2022-10-06 01:03:46 +0200

    Move sprites & Minimize my js & Local copy of Bootstrap & Some cleanup

--- static/script.js
+++
@@ -1,8 +0,0 @@
-function rows_filter(query) {
-  document.querySelectorAll("tr").forEach(function (tr) {
-      if (!tr.className.includes(query))
-        tr.style = "display: none;"
-      else
-        tr.style = ""
-  })
-}
