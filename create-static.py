6f6dae0f2afd471aa43c34bf1599f987dcc073c2
elpekenin <monopatinct@gmail.com>
2022-10-06 17:30:01 +0200

    Add missing script

---
+++ create-static.py
@@ -0,0 +1,21 @@
+from main import *
+from pathlib import Path
+
+app.config.update(
+    SERVER_NAME="elpekenin.tk",
+    APPLICATION_ROOT="."
+)
+
+print("Creating static HTMLs")
+
+Path("static/html/40dex/").mkdir(parents=True, exist_ok=True) # create output dir if doesn't exist already
+
+with app.app_context():
+    for i in range(1, 9):
+        get_40dex_page(i)
+        print(f"==> Finished 40dex/{i}.html")
+
+    get_40dex_stats_page()
+    print("==> Finished 40dex/stats.html")
+
+print("==> ==> Done")
